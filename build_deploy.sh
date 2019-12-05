efd989df06412f6b1ed797c4cb5d0b5635ba80d7
Rob Rati <rrati@redhat.com>
2019-12-05 08:47:43 -0500

    Properly templated deployment yamls

--- build_deploy.sh
+++ build_deploy.sh
@@ -3,8 +3,8 @@
 set -ex
 
 QUAY="quay.io/app-sre"
-VERSION=`grep 'ENV SENTRY_VERSION' Dockerfile | cut -d' ' -f 3 | tr -d '\r\n'`
-TARGET="${QUAY}/sentry:${VERSION}"
+TAG=$(shell git rev-parse --short=7 HEAD)
+TARGET="${QUAY}/sentry:${TAG}"
 
 docker build --pull -t ${TARGET} .
 docker push ${TARGET}
